84c94cd50d1cf3bd2c51fce9709f97c99e886cb1
Ricardo <ricarditomontserrat@gmail.com>
2023-09-08 07:24:24 +0200

    Shorter input for the lols

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ pygame.display.set_caption("Ninja Saga")
 
 
 class Key(IntEnum):
-    UP = 0
-    DOWN = 1
-    LEFT = 2
-    RIGHT = 3
+    UP = pygame.K_UP - pygame.K_RIGHT  # 3
+    DOWN = pygame.K_DOWN - pygame.K_RIGHT  # 2
+    LEFT = pygame.K_LEFT - pygame.K_RIGHT  # 1
+    RIGHT = pygame.K_RIGHT - pygame.K_RIGHT  # 0
 
 
 class Player:
@@ -42,38 +42,20 @@ while True:
         if event.type == pygame.QUIT:
             close_window()
         elif event.type == pygame.KEYDOWN:
-            match event.key:
-                case pygame.K_UP | pygame.K_w:
-                    player.movement[Key.UP] = 5
-                case pygame.K_DOWN | pygame.K_s:
-                    player.movement[Key.DOWN] = 5
-                case pygame.K_LEFT | pygame.K_a:
-                    player.movement[Key.LEFT] = 5
-                case pygame.K_RIGHT | pygame.K_d:
-                    player.movement[Key.RIGHT] = 5
-                case pygame.K_ESCAPE:
-                    close_window()
-                case _:
-                    pass
+            if event.key in (pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT):
+                player.movement[event.key - pygame.K_RIGHT] = 5
+            elif event.key == pygame.K_ESCAPE:
+                close_window()
         elif event.type == pygame.KEYUP:
-            match event.key:
-                case pygame.K_UP | pygame.K_w:
-                    player.movement[Key.UP] = 0
-                case pygame.K_DOWN | pygame.K_s:
-                    player.movement[Key.DOWN] = 0
-                case pygame.K_LEFT | pygame.K_a:
-                    player.movement[Key.LEFT] = 0
-                case pygame.K_RIGHT | pygame.K_d:
-                    player.movement[Key.RIGHT] = 0
-                case _:
-                    pass
+            if event.key in (pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT):
+                player.movement[event.key - pygame.K_RIGHT] = 0
 
     player.position[1] += player.movement[Key.DOWN] - player.movement[Key.UP]
     player.position[0] += player.movement[Key.RIGHT] - player.movement[Key.LEFT]
 
     screen.fill((0x5b, 0xb4, 0xdd))  # 0x5bb4dd
-    
-    player_collider = pygame.Rect(*player.position, *player.img.get_size()) # type: ignore
+
+    player_collider = pygame.Rect(*player.position, *player.img.get_size())  # type: ignore
     red = round(0xFF * 0.5) if player_collider.colliderect(collision_zone) else 0xFF
     pygame.draw.rect(screen, (red, 0, 0), collision_zone)
 
